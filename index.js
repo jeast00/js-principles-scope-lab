f351995865d5d2e0e4822ef4c2ab4ef37d0912ca
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-02-20 12:30:50 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'new bob';
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'old bob';
+}
+
+
